1abc2fd325ebb9d9a96026c818f33f3172ae6819
Altraoz <curresty@unal.edu.co>
2025-10-26 15:15:28 -0500

    mergin

--- p1_f.c
+++ p1_f.c
@@ -1,5 +1,5 @@
 //este es el "bueno"
-//assadasd
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,14 +11,15 @@
 #include <limits.h>
 #include <string.h>
 
-#define SHM_NOM "/mem_compartida"
-#define SEM_EMPTY "/sem_1" // /sem_empty
-#define SEM_FULL "/sem_2"   // /sem_full
-#define SEM_MUTEX "/sem_3" // /sem_mutex
+#define SHM_NAME "/buffer_compartido"
+#define SEM_EMPTY "/sem_empty"
+#define SEM_FULL "/sem_full"
+#define SEM_MUTEX "/sem_mutex"
 
 typedef struct {
 
     int value;
+
 } shared_data;
 
 #include <sys/wait.h>
@@ -38,7 +39,7 @@ int parse_int(const char *arg, const char *nombre, long min, long max){
 
 
 void fibonacci(int a1, int a2, int n, shared_data *data,
-               sem_t *sem_1, sem_t *sem_2, sem_t *sem_3) {
+               sem_t *sem_empty, sem_t *sem_full, sem_t *sem_mutex) {
 
     for (int i = 0; i < n; i++) {
 
@@ -53,20 +54,20 @@ void fibonacci(int a1, int a2, int n, shared_data *data,
 
         }
 
-        sem_wait(sem_1);
-        sem_wait(sem_3);
+        sem_wait(sem_empty);
+        sem_wait(sem_mutex);
 
         data->value = val;
 
-        sem_post(sem_3);
-        sem_post(sem_2);
+        sem_post(sem_mutex);
+        sem_post(sem_full);
     }
 
-    sem_wait(sem_1);
-    sem_wait(sem_3);
+    sem_wait(sem_empty);
+    sem_wait(sem_mutex);
     data->value = -1;
-    sem_post(sem_3);
-    sem_post(sem_2);
+    sem_post(sem_mutex);
+    sem_post(sem_full);
 }
 
 void power_of_two(int a3, int n, shared_data *data,
@@ -94,109 +95,42 @@ void power_of_two(int a3, int n, shared_data *data,
 
 int main(int argc, char *argv[]) {
     if (argc != 5) {
-        
         fprintf(stderr, "Uso: %s N a1 a2 a3\n", argv[0]);
         return 1;
-
     }
 
     char *endptr;
     long temp;
     int N, a1, a2, a3;
 
-    //Error N
-    errno = 0;
-    temp = strtol(argv[1], &endptr, 10);
-    if (errno != 0 || *endptr != '\0' || temp < 0 || temp > INT_MAX) {
-        fprintf(stderr, "N debe ser un entero.\n");
-        return 1;
-    }
-    N = (int)temp;
+    int N = parse_int(argv[1], "N", 0, INT_MAX);
+    int a1 = parse_int(argv[2], "a1", INT_MIN, INT_MAX);
+    int a2 = parse_int(argv[3], "a2", INT_MIN, INT_MAX);
+    int a3 = parse_int(argv[4], "a3", INT_MIN, INT_MAX);
 
-    //Error a1
-    errno = 0;
-    temp = strtol(argv[2], &endptr, 10);
-    if (errno != 0 || *endptr != '\0' || temp > INT_MAX || temp < INT_MIN) {
-        fprintf(stderr, "a1 debe ser un entero.\n");
-        return 1;
-    }
-    a1 = (int)temp;
+    sem_t *sem_empty = sem_open(SEM_EMPTY, 0);
+    if (sem_empty == SEM_FAILED) {
 
-    //Error a2
-    errno = 0;
-    temp = strtol(argv[3], &endptr, 10);
-    if (errno != 0 || *endptr != '\0' || temp > INT_MAX || temp < INT_MIN) {
-        fprintf(stderr, "a2 debe ser un entero.\n");
-        return 1;
-    }
-    a2 = (int)temp;
-
-    //Error a3
-    errno = 0;
-    temp = strtol(argv[4], &endptr, 10);
-    if (errno != 0 || *endptr != '\0' || temp > INT_MAX || temp < INT_MIN) {
-        fprintf(stderr, "a3 debe ser un entero.\n");
-        return 1;
-    }
-    a3 = (int)temp;
-
-    //int N = atoi(argv[1]); estos no detectan errores
-    //int a1 = atoi(argv[2]);
-    //int a2 = atoi(argv[3]);
-    //int a3 = atoi(argv[4]);
-
-    sem_t *sem_1, *sem_2, *sem_3;
-    int val_1, val_2, val_3; // val_empty, full, y mutex como los sem_
-
-    //intenta abrir los semáforos
-    sem_1 = sem_open(SEM_EMPTY, 0);
-    sem_2  = sem_open(SEM_FULL, 0);
-    sem_3 = sem_open(SEM_MUTEX, 0);
-
-    if (sem_1 == SEM_FAILED || sem_2 == SEM_FAILED || sem_3 == SEM_FAILED) {
-
-        perror("Error al abrir semáforos");
-        fprintf(stderr, "P3 o P4 no están en ejecución.\n");
-        exit(EXIT_FAILURE);
+        printf("P3 o P4 no están en ejecución.\n");
+        exit(1);
 
     }
 
-    //verifica con getvalue
-    if (sem_getvalue(sem_1, &val_1) == -1 ||
-        sem_getvalue(sem_2, &val_2) == -1 ||
-        sem_getvalue(sem_3, &val_3) == -1) {
+    sem_t *sem_full  = sem_open(SEM_FULL, 0);
+    sem_t *sem_mutex = sem_open(SEM_MUTEX, 0);
 
-        perror("Error sem_getvalue");
-        fprintf(stderr, "P3 o P4 estén en ejecución.\n");
-        exit(EXIT_FAILURE);
+    int shm_fd = shm_open(SHM_NAME, O_RDWR, 0666);
 
-    }
+    if (shm_fd == -1) { 
 
-    //si val es invalido, no estan bien inicializados
-    if (val_1 < 0 || val_2 < 0 || val_3 < 0) {
-
-        fprintf(stderr, "P3 o P4 no están correctamente inicializados.\n");
-        exit(EXIT_FAILURE);
-
-    }
-
-    //inicializar shm_fd como mem
-
-    int mem;     
-    mem = shm_open(SHM_NOM, O_CREAT | O_RDWR, 0666);
-    if (mem == -1) {
-
-        perror("Error shm_open");
-        return 1;
+        perror("shm_open"); exit(1); 
 
     }
 
     shared_data *data = mmap(NULL, sizeof(shared_data),
-                              PROT_READ | PROT_WRITE, MAP_SHARED, mem, 0);
+                              PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
     if (data == MAP_FAILED) { 
-
         perror("mmap"); exit(1); 
-
     }
     
     pid_t pid = fork();
@@ -210,7 +144,6 @@ int main(int argc, char *argv[]) {
         perror("execl");
         exit(1);
     } else {
-
         fibonacci(a1, a2, N, data, sem_empty, sem_full, sem_mutex);
         int status;
         waitpid(pid, &status, 0);
@@ -219,10 +152,10 @@ int main(int argc, char *argv[]) {
     }
 
     munmap(data, sizeof(shared_data));
-    close(mem);
-    sem_close(sem_1);
-    sem_close(sem_2);
-    sem_close(sem_3);
+    close(shm_fd);
+    sem_close(sem_empty);
+    sem_close(sem_full);
+    sem_close(sem_mutex);
 
     return 0;
 }
