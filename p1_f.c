2d592a9a6e8a8b54730270a1fe074659505261e2
felipegiraldo322-alt <felipegiraldo322@gmail.com>
2025-10-26 19:54:35 -0500

    .

--- p1_f.c
+++ p1_f.c
@@ -37,7 +37,8 @@ int parse_int(const char *arg, const char *nombre, long min, long max){
 
 void fibonacci(int a1, int a2, int N, shared_data *data,
                sem_t *sem_1, sem_t *sem_2, sem_t *sem_3) {
-
+                
+    int next;
     for (int i = 0; i < N; i++) {
         next = a1 + a2;
         printf("%d", next);
